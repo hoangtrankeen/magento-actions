d87164c5214dd23e846ebff18623f0043a536994
hoang <hoangtrancn@gmail.com>
2020-04-06 22:35:52 +0700

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,8 +6,9 @@ update-alternatives --set php /usr/bin/php${INPUT_PHP}
 
 if [ $INPUT_OVERRIDE_SETTINGS ]
 then
-  cp -R ./config/* /opt/config/
-  cp -R ./scripts/* /opt/scripts/
+  echo $(pwd)
+  cp -R config/* /opt/config/
+  cp -R scripts/* /opt/scripts/
   bash /opt/scripts/${INPUT_PROCESS}.sh
 else
   bash /opt/scripts/${INPUT_PROCESS}.sh

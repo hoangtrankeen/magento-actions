70ea6b4c449843d8b3d64d2c43316a223cae9ea9
hoang <hoangtrancn@gmail.com>
2020-04-07 18:03:18 +0700

    Update deploy-staging.sh

--- scripts/deploy-staging.sh
+++ scripts/deploy-staging.sh
@@ -2,6 +2,18 @@
 
 set -e
 
+PROJECT_PATH="$(pwd)"
+
+
+echo "project path is $PROJECT_PATH";
+
+which ssh-agent || ( apt-get update -y && apt-get install openssh-client -y )
+eval $(ssh-agent -s)
+mkdir ~/.ssh/ && echo "$SSH_PRIVATE_KEY" > ~/.ssh/id_rsa && chmod 600 ~/.ssh/id_rsa
+ssh-add ~/.ssh/id_rsa
+echo "$SSH_CONFIG" > /etc/ssh/ssh_config && chmod 600 /etc/ssh/ssh_config
+
+
 echo "Move sites updated code to staging site"
 rsync -e "ssh -o StrictHostKeyChecking=no -p 22" -avz ./ glassesgallery-test-ssh@34.197.214.188:/home/cloudpanel/htdocs/test.glassesgallery.com/
 ssh -o StrictHostKeyChecking=no -p 22 glassesgallery-test-ssh@34.197.214.188 'bash /home/cloudpanel/htdocs/test.glassesgallery.com/staging.sh'
